f75afb206e4b44242ad31702d8062962f058c68b
Ahmed Mohamed <mohamed@kuicr.kyoto-u.ac.jp>
2016-01-26 00:55:17 +0900

    New url pattern to remove django 110 warning

--- django_nmrpro/urls.py
+++ django_nmrpro/urls.py
@@ -1,14 +1,14 @@
 from django.conf.urls import patterns, include, url
+from django_nmrpro import views
 
-
-urlpatterns = patterns('',
-    url(r'^plugins$', 'django_nmrpro.views.proc_plugin'),
-    url(r'^menu$', 'django_nmrpro.views.menu'),
-    url(r'^spectrum/(?P<url>\S+)$', 'django_nmrpro.views.spec_url'),
-    url(r'^viewSpectrum/(?P<url>\S+)$', 'django_nmrpro.views.view_spectrum'),
+urlpatterns = [
+    url(r'^plugins$', views.proc_plugin),
+    url(r'^menu$', views.menu),
+    url(r'^spectrum/(?P<url>\S+)$', views.spec_url),
+    url(r'^viewSpectrum/(?P<url>\S+)$', views.view_spectrum),
     
     # demo urls. Comment if not needed
-    url(r'^coffees_specs$', 'django_nmrpro.views.coffees_test'),
-    url(r'^nmrpro_test/$', 'django_nmrpro.views.coffees_view'),
-)
+    url(r'^coffees_specs$', views.coffees_test),
+    url(r'^nmrpro_test/$', views.coffees_view),
+]
 
